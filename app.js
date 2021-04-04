fd16c4ded9a764c53ba3656ffa746a7dd476fa54
al-moreno <77991840+al-moreno@users.noreply.github.com>
2021-04-04 03:41:28 -0500

    first commit
    Adding app.js and index.js.

---
+++ app.js
@@ -0,0 +1,62 @@
+
+const path = require("path");
+const fs = require("fs");
+
+const inquirer = require("inquirer");
+
+const Manager = require("./js/Manager");
+const Engineer = require("./js/Engineer");
+const Intern = require("./js/Intern");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./js/htmlRenderer");
+const { listenerCount } = require("process");
+
+var employeesArray = [];
+
+const {
+  addEmployee,
+  managerPrompt,
+  engineerPrompt,
+  internPrompt,
+} = require("./index");
+
+
+async function getEmployees() {
+  let proceed = true,
+    choice,
+    engineer,
+    intern;
+  while (proceed) {
+    choice = await addEmployee();
+    switch (choice.engineerOrIntern.toUpperCase()) {
+      case "ENGINEER":
+        engineer = await engineerPrompt();
+        employeesArray.push(
+          new Engineer(engineer.name, engineer.id, engineer.email,engineer.github
+          )
+        );
+        break;
+      case "INTERN":
+        intern = await internPrompt();
+        employeesArray.push(
+          new Intern(intern.name, intern.id, intern.email, intern.school)
+        );
+        break;
+      default:
+        proceed = false;
+        fs.writeFile("index.html", render(employeesArray), function () {
+          console.log("HTML file created!");
+        });
+    }
+  }
+  return true;
+}
+async function main() {
+  const manager = await managerPrompt();
+  employeesArray.push(
+    new Manager(manager.name, manager.id, manager.email, manager.officeNumber)
+  );
+  await getEmployees();
+}
+main();
---
+++ index.js
@@ -0,0 +1,91 @@
+
+const inquirer = require("inquirer");
+
+async function addEmployee() {
+  return inquirer.prompt([
+    {
+      type: "list",
+      message: "who else would you like to add to the team?",
+      name: "engineerOrIntern",
+      choices: ["Engineer", "Intern", "Exit"],
+    },
+  ]);
+}
+async function managerPrompt() {inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the manager's employee id number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the managers email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the managers's office number?",
+      name: "officeNumber",
+    },
+  ]);
+}
+async function engineerPrompt() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the engineer's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's id number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the engineer's GITHUB username?",
+      name: "github",
+    },
+  ]);
+}
+async function internPrompt() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the intern's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the intern's ID number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the intern's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What school did the intern attend?",
+      name: "school",
+    },
+  ]);
+}
+
+module.exports = {
+  addEmployee,
+  managerPrompt,
+  engineerPrompt,
+  internPrompt,
+};
